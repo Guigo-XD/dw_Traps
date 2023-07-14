9d2c23fd9f2dfd57bbc0ade50c4349207806a7b0
Guigo-XD <guilherme.coelho04@gmail.com>
2023-07-14 19:53:03 -0300

    itens

--- config.lua
+++ config.lua
@@ -6,3 +6,36 @@ Config.Baits= {"cornseed","wheatseed",}
 
 Config.TimeToHarvest = 10
 
+
+---itens
+--[[
+
+["fishtrap"] =
+    {
+        label = "fishtrap",
+        description = "Armadilha para peixe",
+        weight = 0.001,
+        canBeDropped = true,
+        canBeUsed = true,
+        requireLvl = 0,
+        limit = 50,
+        imgsrc = "items/trap.png",
+        type = "item_standard",
+    },
+    ["smallfish"] =
+    {
+        label = "smallfish",
+        description = "peixe",
+        weight = 0.100,
+        canBeDropped = true,
+        canBeUsed = true,
+        requireLvl = 0,
+        limit = 50,
+        imgsrc = "items/provision_fish_bluegill.png",
+        type = "item_standard",
+    },
+
+
+
+]]
+
